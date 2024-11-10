69c68c2403f1236b55a8db7bf0277d1683501532
yaboyfreire <92168759+yaboyfreire@users.noreply.github.com>
2024-11-10 20:20:37 +0000

    Update MainActivity.kt

--- app/src/main/java/pt/iade/ei/martim/rodrigo/SoundMarket/MainActivity.kt
+++ app/src/main/java/pt/iade/ei/martim/rodrigo/SoundMarket/MainActivity.kt
@@ -78,7 +78,8 @@ fun HomeScreen() {
 
                 HorizontalCarousel(
                 items = listOf("Item 1", "Item 2", "Item 3"),
-                text = "Trending"
+                text = "Trending",
+                    ImageURLs = listOf("https://luis.studentsmagestil.pt/wp-content/uploads/2022/06/Swimming_Album_Cover-600x600.jpeg","https://luis.studentsmagestil.pt/wp-content/uploads/2022/06/Swimming_Album_Cover-600x600.jpeg","https://luis.studentsmagestil.pt/wp-content/uploads/2022/06/Swimming_Album_Cover-600x600.jpeg")
                 )
 
                 HomeGenreList(items = genreItems) { clickedItem ->
