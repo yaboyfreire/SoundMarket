3f094f3cbc518a9dfdc49f398afaf31eb78f9b3d
Martim Conceição <149968531+MartimConceicao@users.noreply.github.com>
2024-11-24 12:17:48 +0000

    Database create, populate and queries

---
+++ Database/populate.sql
@@ -0,0 +1,96 @@
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('João Silva','Male', '1989-03-25', 'joaosilva', 'joaosilva@gmail.com', 'Portugal', 'hashedpassword1');  #pass1
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Maria Costa','Female', '2000-07-17', 'mariacosta', 'mariacosta@gmail.com', 'Portugal','hashedpassword2');  #pass2
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Carlos Mendes','Male', '2002-12-05', 'carlosmendes', 'carlosmendes@gmail.com', 'Portugal','hashedpassword3');  #pass3
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Ana Rodríguez','Female', '2004-01-09', 'anarodriguez', 'anarodriguez@gmail.com', 'Spain','hashedpassword4');  #pass4
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Luis Pérez','Male', '1989-04-11', 'luisperez', 'luisperez@gmail.com', 'Spain','hashedpassword5');  #pass5
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Sofia Gómez','Female', '1995-09-22', 'sofiagomez', 'sofiagomez@gmail.com', 'Spain','hashedpassword6');  #pass6
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Jean Dupont','Male', '1987-11-03', 'jeandupont', 'jeandupont@gmail.com', 'France','hashedpassword7');  #pass7
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Lucie Martin','Female', '1991-05-21', 'luciemartin', 'luciemartin@gmail.com', 'France','hashedpassword8');  #pass8
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Thomas Müller','Male', '2001-06-18', 'thomasmueller', 'thomasmueller@gmail.com', 'Germany','hashedpassword9');  #pass9
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Giulia Ferrari','Female', '2000-02-14', 'giuliaferrari', 'giuliaferrari@gmail.com', 'Italy','hashedpassword10');  #pass10
+insert into utilizador (user_name, user_gender, user_bday, user_username, user_email, user_country, user_passwordhash) values ('Rui Carvalho','Male', '1999-04-24', 'ruicarvalho', 'ruicarvalho@gmail.com', 'Portugal','hashedpassword11');  #pass11
+
+insert into administrator (admin_user_id) values (1);
+
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (1, 'New', 'vinyl', 'Swimming', 'Mac Miller', 'Hip Hop', '2024-11-20 14:00:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (2, 'Very Good', 'cd', 'Swimming', 'Mac Miller', 'Hip Hop', '2024-11-19 16:30:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (3, 'Good', 'vinyl', 'To Pimp a Butterfly', 'Kendrick Lamar', 'Hip Hop', '2024-11-18 10:15:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (4, 'Used', 'cd', 'Lemonade', 'Beyoncé', 'Pop', '2024-11-17 18:45:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (5, 'Damaged', 'vinyl', 'Dark Side of the Moon', 'Pink Floyd', 'Rock', '2024-11-16 09:30:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (6, 'New', 'vinyl', 'Random Access Memories', 'Daft Punk', 'Electronic', '2024-11-15 13:00:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (7, 'Very Good', 'cd', 'Thriller', 'Michael Jackson', 'Pop', '2024-11-14 12:00:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (8, 'Good', 'vinyl', 'Abbey Road', 'The Beatles', 'Rock', '2024-11-13 11:30:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (9, 'Used', 'cd', 'The Dark Knight OST', 'Hans Zimmer', 'Soundtrack', '2024-11-12 10:00:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (10, 'Damaged', 'vinyl', 'Back In Black', 'AC/DC', 'Rock', '2024-11-11 15:00:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (1, 'Very Good', 'cd', 'In the Court of the Crimson King', 'King Crimson', 'Progressive Rock', '2024-11-10 14:45:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (2, 'Good', 'vinyl', 'The Wall', 'Pink Floyd', 'Rock', '2024-11-09 16:00:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (3, 'Used', 'cd', '21', 'Adele', 'Pop', '2024-11-08 10:20:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (4, 'Damaged', 'vinyl', 'The Joshua Tree', 'U2', 'Rock', '2024-11-07 18:00:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (5, 'New', 'cd', 'The Life of Pablo', 'Kanye West', 'Hip Hop', '2024-11-06 13:50:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (6, 'Very Good', 'vinyl', 'Hotel California', 'Eagles', 'Rock', '2024-11-05 12:30:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (7, 'Good', 'cd', 'Good Kid, M.A.A.D City', 'Kendrick Lamar', 'Hip Hop', '2024-11-04 15:20:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (8, 'Used', 'vinyl', 'A Night at the Opera', 'Queen', 'Rock', '2024-11-03 14:10:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (9, 'Damaged', 'cd', 'Graduation', 'Kanye West', 'Hip Hop', '2024-11-02 09:40:00');
+insert into album (album_user_id, album_condition, album_format, album_title, album_artist, album_genre, album_added_date) values (10, 'New', 'vinyl', 'The Dark Side of the Moon', 'Pink Floyd', 'Rock', '2024-11-01 08:50:00');
+
+
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (1, 1, '2024-11-20 14:00:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (2, 2, '2024-11-19 16:30:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (3, 3, '2024-11-18 10:15:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (4, 4, '2024-11-17 18:45:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (5, 5, '2024-11-16 09:30:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (6, 1, '2024-11-15 13:00:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (7, 2, '2024-11-14 12:00:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (8, 3, '2024-11-13 11:30:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (9, 4, '2024-11-12 10:00:00');
+insert into wishlist (wl_user_id, wl_album_id, wl_added_date) values (10, 5, '2024-11-11 15:00:00');
+
+insert into selling (selling_user_id, selling_album_id, selling_description, selling_price, selling_created_at) values (1, 1, 'Brand new vinyl in excellent condition', 29.99, '2024-11-20 14:00:00');
+insert into selling (selling_user_id, selling_album_id, selling_description, selling_price, selling_created_at) values (2, 4, 'Very good condition CD, almost like new', 19.99, '2024-11-19 16:30:00');
+insert into selling (selling_user_id, selling_album_id, selling_description, selling_price, selling_created_at) values (3, 3, 'Slightly used vinyl, great sound', 22.50, '2024-11-18 10:15:00');
+insert into selling (selling_user_id, selling_album_id, selling_description, selling_price, selling_created_at) values (4, 4, 'Used CD with minor scratches but works fine', 15.00, '2024-11-17 18:45:00');
+insert into selling (selling_user_id, selling_album_id, selling_description, selling_price, selling_created_at) values (5, 10, 'Damaged vinyl, cover worn but playable', 10.00, '2024-11-16 09:30:00');
+insert into selling (selling_user_id, selling_album_id, selling_description, selling_price, selling_created_at) values (6, 12, 'Brand new vinyl, factory sealed', 35.00, '2024-11-15 13:00:00');
+insert into selling (selling_user_id, selling_album_id, selling_description, selling_price, selling_created_at) values (7, 7, 'Very good condition CD, no scratches', 18.50, '2024-11-14 12:00:00');
+
+insert into buy (buy_selling_id, buy_buyer_id, buy_price_paid, buy_status_id, buy_created_at) values (1, 2, 29.99, 1, '2024-11-20 15:30:00');
+insert into buy (buy_selling_id, buy_buyer_id, buy_price_paid, buy_status_id, buy_created_at) values (3, 4, 22.50, 1, '2024-11-19 10:45:00');
+insert into buy (buy_selling_id, buy_buyer_id, buy_price_paid, buy_status_id, buy_created_at) values (5, 7, 10.00, 2, '2024-11-18 14:30:00');
+insert into buy (buy_selling_id, buy_buyer_id, buy_price_paid, buy_status_id, buy_created_at) values (7, 9, 18.50, 1, '2024-11-17 16:00:00');
+insert into buy (buy_selling_id, buy_buyer_id, buy_price_paid, buy_status_id, buy_created_at) values (4, 6, 15.00, 1, '2024-11-16 12:15:00');
+insert into buy (buy_selling_id, buy_buyer_id, buy_price_paid, buy_status_id, buy_created_at) values (2, 5, 19.99, 2, '2024-11-15 09:00:00');
+insert into buy (buy_selling_id, buy_buyer_id, buy_price_paid, buy_status_id, buy_created_at) values (6, 3, 35.00, 1, '2024-11-14 11:00:00');
+
+insert into chat (chat_topic, chat_seller_id, chat_buyer_id, chat_started_at) values ('Swimming', 1, 2, '2024-11-20 15:30:00');
+insert into chat (chat_topic, chat_seller_id, chat_buyer_id, chat_started_at) values ('To Pimp a Butterfly', 3, 4, '2024-11-19 10:45:00');
+insert into chat (chat_topic, chat_seller_id, chat_buyer_id, chat_started_at) values ('Dark Side of the Moon', 5, 7, '2024-11-18 14:30:00');
+insert into chat (chat_topic, chat_seller_id, chat_buyer_id, chat_started_at) values ('Thriller', 7, 9, '2024-11-17 16:00:00');
+insert into chat (chat_topic, chat_seller_id, chat_buyer_id, chat_started_at) values ('Lemonade', 4, 6, '2024-11-16 12:15:00');
+insert into chat (chat_topic, chat_seller_id, chat_buyer_id, chat_started_at) values ('Random Access Memories', 6, 5, '2024-11-15 09:00:00');
+insert into chat (chat_topic, chat_seller_id, chat_buyer_id, chat_started_at) values ('Swimming', 2, 3, '2024-11-14 11:00:00');
+
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (1, 1, 'Hey, how can I help you with the album?', '2024-11-20 15:45:00', 2);
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (1, 2, 'I am interested in buying it. Is it still available?', '2024-11-20 15:50:00', 1);
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (2, 3, 'I am interested in the vinyl. Is it in good condition?', '2024-11-19 10:50:00', 4);
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (3, 5, 'The vinyl is a bit damaged, but still playable. Would you like to proceed?', '2024-11-18 14:45:00', 7);
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (4, 7, 'Yes, I am willing to buy the CD. Can you ship it?', '2024-11-17 16:10:00', 9);
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (5, 4, 'The CD is used but still in great condition. Are you still interested?', '2024-11-16 12:20:00', 6);
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (6, 6, 'I just received the payment, preparing the shipment now.', '2024-11-15 09:05:00', 5);
+insert into message (msg_chat_id, msg_sender_id, msg_text, msg_sent_at, msg_receiver_id) values (7, 2, 'Yes, the vinyl is available. Do you want it shipped?', '2024-11-14 11:15:00', 3);
+
+insert into state (state_buy_id, state_type, state_created_at) values (1, 'Paid', '2024-11-20 15:35:00');
+insert into state (state_buy_id, state_type, state_created_at) values (2, 'Paid', '2024-11-19 10:50:00');
+insert into state (state_buy_id, state_type, state_created_at) values (3, 'Waiting for payment', '2024-11-18 14:40:00');
+insert into state (state_buy_id, state_type, state_created_at) values (4, 'Paid', '2024-11-17 16:05:00');
+insert into state (state_buy_id, state_type, state_created_at) values (5, 'Shipped', '2024-11-16 12:20:00');
+insert into state (state_buy_id, state_type, state_created_at) values (6, 'Waiting for payment', '2024-11-15 09:10:00');
+insert into state (state_buy_id, state_type, state_created_at) values (7, 'Shipped', '2024-11-14 11:20:00');
+
+insert into buy_state (bs_buy_id, bs_state_id, bs_created_at) values (1, 1, '2024-11-20 15:35:00');
+insert into buy_state (bs_buy_id, bs_state_id, bs_created_at) values (2, 2, '2024-11-19 10:50:00');
+insert into buy_state (bs_buy_id, bs_state_id, bs_created_at) values (3, 3, '2024-11-18 14:40:00');
+insert into buy_state (bs_buy_id, bs_state_id, bs_created_at) values (4, 4, '2024-11-17 16:05:00');
+insert into buy_state (bs_buy_id, bs_state_id, bs_created_at) values (5, 5, '2024-11-16 12:20:00');
+insert into buy_state (bs_buy_id, bs_state_id, bs_created_at) values (6, 6, '2024-11-15 09:10:00');
+insert into buy_state (bs_buy_id, bs_state_id, bs_created_at) values (7, 7, '2024-11-14 11:20:00');
+
